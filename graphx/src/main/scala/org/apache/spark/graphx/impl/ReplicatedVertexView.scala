7bf46df7099a423510bf6461a115b67090c083dc
Michael Mior <mmior@cs.rit.edu>
2018-10-20 04:44:27 -0400

    Track use in ReplicatedVertexView

--- graphx/src/main/scala/org/apache/spark/graphx/impl/ReplicatedVertexView.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/impl/ReplicatedVertexView.scala
@@ -61,8 +61,6 @@ class ReplicatedVertexView[VD: ClassTag, ED: ClassTag](
   def upgrade(vertices: VertexRDD[VD], includeSrc: Boolean, includeDst: Boolean) {
     val shipSrc = includeSrc && !hasSrcId
     val shipDst = includeDst && !hasDstId
-    edges.partitionsRDD.stopTrackingUse()
-    edges.stopTrackingUse()
     if (shipSrc || shipDst) {
       val shippedVerts: RDD[(Int, VertexAttributeBlock[VD])] =
         vertices.shipVertexAttributes(shipSrc, shipDst)
@@ -75,8 +73,6 @@ class ReplicatedVertexView[VD: ClassTag, ED: ClassTag](
             (pid, edgePartition.updateVertices(shippedVertsIter.flatMap(_._2.iterator)))
         }
       })
-      edges.partitionsRDD.resumeTrackingUse()
-      edges.resumeTrackingUse()
       edges = newEdges
       hasSrcId = includeSrc
       hasDstId = includeDst
